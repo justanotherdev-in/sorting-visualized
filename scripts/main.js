421eb83d95043b9e5de3ede3b76925517dbacde0
justanotherdev-in <admin@justanotherdev.in>
2023-03-05 15:19:53 +0530

    add selection sort

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
+let sortingType = 'bubble';
 const sortingBox = document.querySelector('#sorting_box');
 
 addNodes(50);
+
+function setSortingType(type) {
+    sortingType = type;
+}
 
 function addNodes(value) {
     sortingBox.innerHTML = '';
@@ -14,6 +19,15 @@ function addNodes(value) {
     }
 }
 
+async function sort() {
+    switch (sortingType) {
+        case 'bubble':
+            return bubbleSort();
+        case 'selection':
+            return selectionSort();
+    }
+}
+
 async function bubbleSort() {
     const domNodes = document.querySelectorAll('.sorting_element');
     const arr = [];
@@ -23,23 +37,13 @@ async function bubbleSort() {
     for(let i=0; i<arr.length; i++) {
         let swap = false;
         for(let j=0; j<arr.length - 1 - i; j++) {
-            const h1 = arr[j].val;
             domNodes[j].style.backgroundColor = '#3d5a80';
             domNodes[j+1].style.backgroundColor = '#293241';
             await delay(0.1);
-            if (h1 > arr[j+1].val) {
-                domNodes[j].hidden = true;
-                domNodes[j+1].hidden = true;
-                await delay(0.01);
-                domNodes[j].setAttribute('style', `height:${arr[j+1].val}px`);
-                domNodes[j+1].setAttribute('style', `height:${h1}px`);
-                domNodes[j].hidden = false;
-                domNodes[j+1].hidden = false;
-                arr[j].val = arr[j + 1].val;
-                arr[j+1].val = h1;
+            if (arr[j].val > arr[j+1].val) {
+                await swapNodes(domNodes, arr, j, j + 1);
                 swap = true;
             }
-            await delay(0.1);
             domNodes[j].style.backgroundColor = '#ee6c4d';
             domNodes[j+1].style.backgroundColor = '#ee6c4d';
         }
@@ -48,6 +52,48 @@ async function bubbleSort() {
         }
     }
     // console.log(arr);
+}
+
+async function selectionSort() {
+    const domNodes = document.querySelectorAll('.sorting_element');
+    const arr = [];
+    domNodes.forEach(node => arr.push({ id: node.id, val: parseInt(node.style.height) }));
+    // console.log(arr);
+    for(let i=0; i<arr.length; i++) {
+        let minInd = i;
+        for(let j=i + 1; j < arr.length; j++) {
+            domNodes[i].style.backgroundColor = '#3d5a80';
+            domNodes[j].style.backgroundColor = '#293241';
+            await delay(0.1);
+            if (arr[minInd].val > arr[j].val) {
+                minInd = j;
+            }
+            domNodes[j].style.backgroundColor = '#ee6c4d';
+        }
+        if (minInd === i) {
+            domNodes[i].style.backgroundColor = '#ee6c4d';
+            domNodes[minInd].style.backgroundColor = '#ee6c4d';
+            continue;
+        }
+        await swapNodes(domNodes, arr, i, minInd);
+        domNodes[i].style.backgroundColor = '#ee6c4d';
+        domNodes[minInd].style.backgroundColor = '#ee6c4d';
+    }
+}
+
+async function swapNodes(domNodes, arr, i, j) {
+    domNodes[j].style.backgroundColor = '#293241';
+    domNodes[i].hidden = true;
+    domNodes[j].hidden = true;
+    await delay(0.01);
+    domNodes[i].setAttribute('style', `height:${arr[j].val}px`);
+    domNodes[j].setAttribute('style', `height:${arr[i].val}px`);
+    domNodes[i].hidden = false;
+    domNodes[j].hidden = false;
+    const temp = arr[j].val;
+    arr[j].val = arr[i].val;
+    arr[i].val = temp;
+    await delay(0.1);
 }
 
 function delay(n) {
